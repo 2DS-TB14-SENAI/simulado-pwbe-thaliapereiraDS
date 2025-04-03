10dc9a00a57d88e84b587062280c400b6c063fc0
Thalia Pereira Modolo <38630444878@>
2025-04-03 16:05:43 -0300

    teste

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('livros.urls')),
-    path('', include('usuarios.urls')),
+    path('', include('livros.urls')),    
+    path('x', include('usuarios.urls')),  
 ]
--- livros/urls.py
+++ livros/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_livros
+from .views import listar_livros, api_livros
 
 urlpatterns = [
-    path('livros/', listar_livros, name='listar_livros'),
+    path('livros/', listar_livros, name='listar_livros'),  # Rota para o template
+    path('api/livros/', api_livros, name='api_livros'),     # Rota para a API
 ]
